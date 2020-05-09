e8e0964928f9c9e7a0b56ea5344ccae032dc784f
Ioannis Koutsoukos <kutsukos@csd.uoc.gr>
2020-05-09 22:13:36 +0300

    Substring finder and counter update
    faster implementation, with one instead of two for loops

--- Python3/find-a-string.py
+++ Python3/find-a-string.py
@@ -1,21 +1,14 @@
-# https://www.hackerrank.com/challenges/find-a-string/problem
+# link: https://www.hackerrank.com/challenges/find-a-string/problem
 
 def count_substring(string, sub_string):
-    points=0
-    if len(sub_string)>len(string):
+    counter=0
+    if len(sub_string)>len(string):     #if the length of substring is bigger than the string's length, then there is no need to continue
         return 0
-    else:
-        for i in range(0,len(string)-len(sub_string)+1):
-            m_flag=1
-            for y in range(0,len(sub_string)):
-                if(string[i+y]!=sub_string[y]):
-                    m_flag=0
-            if(m_flag==1):
-                points=points+1
-    return points
-
-    
-
+    else:       #checking the string if contains the substring, starting in any possible position
+        for i in range(0,len(string)-len(sub_string)+1):    #substring can start from position 0, to position len(string)-len(substring)
+            if string[i:i+len(sub_string)]==sub_string:     #checking if we found the substring
+                counter=counter+1   
+    return counter
 
 
 if __name__ == '__main__':
